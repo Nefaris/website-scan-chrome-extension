672171c6dae797e73d89a38ff954dc2f06de15e9
Kamil <34476654+Nefaris@users.noreply.github.com>
2020-02-16 00:37:04 +0100

    whitespace fixed

--- application.js
+++ application.js
@@ -4,40 +4,40 @@ const clearInput = document.querySelector('#clearInput');
 const searchForm = document.querySelector('#searchForm');
 
 chrome.tabs.getSelected(null, function (tab) {
-	searchInput.value = tab.url;
+    searchInput.value = tab.url;
 });
 
 clearInput.addEventListener('click', (e) => {
-	e.preventDefault();
-	searchInput.value = '';
-	searchInput.focus();
+    e.preventDefault();
+    searchInput.value = '';
+    searchInput.focus();
 });
 
 searchForm.addEventListener('submit', (e) => {
-	e.preventDefault();
+    e.preventDefault();
 
-	const gtmetrix = 'https://gtmetrix.com/';
-	const pingdom = 'https://tools.pingdom.com/';
-	const insights = 'https://developers.google.com/speed/pagespeed/insights/';
+    const gtmetrix = 'https://gtmetrix.com/';
+    const pingdom = 'https://tools.pingdom.com/';
+    const insights = 'https://developers.google.com/speed/pagespeed/insights/';
 
-	chrome.storage.local.set({
-		domain: searchInput.value
-	});
+    chrome.storage.local.set({
+        domain: searchInput.value
+    });
 
-	chrome.tabs.create({
-		url: gtmetrix,
-		active: false
-	});
+    chrome.tabs.create({
+        url: gtmetrix,
+        active: false
+    });
 
-	chrome.tabs.create({
-		url: pingdom,
-		active: false
-	});
+    chrome.tabs.create({
+        url: pingdom,
+        active: false
+    });
 
-	chrome.tabs.create({
-		url: insights,
-		active: false
-	});
+    chrome.tabs.create({
+        url: insights,
+        active: false
+    });
 
-	window.close();
+    window.close();
 });
--- pingdom.js
+++ pingdom.js
@@ -1,9 +1,9 @@
 chrome.storage.local.get(['domain'], function (result) {
-	if (result.domain != undefined) {
-		document.querySelector('.test-url-input').value = result.domain;
+    if (result.domain != undefined) {
+        document.querySelector('.test-url-input').value = result.domain;
 
-		chrome.storage.local.set({
-			domain: undefined
-		});
-	}
+        chrome.storage.local.set({
+            domain: undefined
+        });
+    }
 });
